2af8b17f72f34eb825ebe4879115d2675ac4d88b
brenndha-cabral <cabralbrenndha@gmail.com>
2025-05-31 22:30:10 -0300

    build: new files

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+        Dictionary<int, string> menu = new Dictionary<int, string>
+        {
+            { 1, "Cadastrar Carro" },
+            { 2, "Remover Carro" },
+            { 3, "Listar Carros" },
+            { 4, "Encontrar Carro" },
+            { 5, "Encerrar Programa!" },
+        };
+
+        Dictionary<int, decimal> values = new Dictionary<int, decimal>
+        {
+            { 1, 10M },
+            { 2, 15M },
+            { 3, 20M },
+            { 4, 25M },
+            { 5, 30M },
+        };
