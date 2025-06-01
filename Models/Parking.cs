7856f4256d5923767a53573ce0d7807cd40381f2
brenndha-cabral <cabralbrenndha@gmail.com>
2025-05-31 22:32:01 -0300

    feat: create IsValidPlate method

--- Models/Parking.cs
+++ Models/Parking.cs
@@ -8,6 +8,13 @@ namespace console_parking_system.Models
 {
     public class Parking
     {
-
+        Dictionary<string, DateTime> carsOnParking = new Dictionary<string, DateTime>{};
+        
+        // Aceita placas antigas (ABC1234) e padrão Mercosul (ABC1D23) - No input para mascarar, está sendo normalizado para maiúsculo
+        private bool IsValidPlate(string plate)
+        {
+            Regex regex = new Regex(@"^[A-Z]{3}[0-9][A-Z0-9][0-9]{2}$", RegexOptions.IgnoreCase);
+            return regex.IsMatch(plate);
+        }
     }
 }
